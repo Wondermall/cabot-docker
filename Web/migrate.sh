d120e4699bd7d709fdf40b0a2f29266f0738175b
Roman Kournjaev <kournjaev@gmail.com>
2014-12-03 17:34:49 +0200

    adding migrate.sh

---
+++ Web/migrate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python manage.py collectstatic --noinput &&\
+python manage.py compress --force &&\
+python manage.py syncdb --noinput && \
+python manage.py migrate && \
+python manage.py loaddata fixture.json 
