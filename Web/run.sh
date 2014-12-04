8d414fa17bf0aa4c62c2c72bbe28787665913941
Roman Kournjaev <kournjaev@gmail.com>
2014-12-04 15:28:44 +0200

    a few final fixes

--- Web/run.sh
+++ Web/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export DATABASE_URL="postgres://docker:docker@$DATABASE_POSTGRE_HOST:5432/docker"
-export CELERY_BROKER_URL="redis://$CELERY_BROKER_REDIS_HOST:6379/1"
-export SES_HOST="$MAIL_EXIM_HOST"
-export SES_USER=""
-export SES_PASS=""
-export SES_PORT="225"
+DATABASE_URL="postgres://docker:docker@$DATABASE_POSTGRE_HOST:5432/docker"
+CELERY_BROKER_URL="redis://$CELERY_BROKER_REDIS_HOST:6379/1"
+SES_HOST="$MAIL_EXIM_HOST"
+SES_USER=""
+SES_PASS=""
+SES_PORT="225"
 
 service nginx restart &&\
 gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
